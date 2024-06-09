321bc80aeee3c7d6a99eed3ebf4c2cb9d8a3f10f
pokkers <karlis@pokkers.lv>
2024-06-10 01:22:38 +0300

    init

---
+++ darttrainer/lang/en/welcome.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'game' => 'Self-motivation darts game training assistant',
+    'game_description' => 'Don\'t you find it boring to choose the sectors yourself to throw a dart at? For me personally
+        - a lot! Therefore, this is a game WHICH WILL DETERMINE which square you have to hit. All you have to do is choose
+         the type of squares you want to throw - regular, double, triple or any. After a certain number of games, accuracy
+        and progress / regression statistics will also be available. If you have any additional questions or suggestions,
+        please contact us using the specified contacts.',
+    'game_step_1' => '1. Choose a sector',
+    'game_step_1_description' => 'Choose the type of game - normal sectors, double sectors, triple sectors or any sector.',
+    'game_step_2' => '2. Ten out of ten',
+    'game_step_2_description' => 'The game will pick a random square one by one from the range you choose. Number of
+    choices (game moves): 10. For each game move, the maximum number of shot attempts: 10. The number of shots needed
+    to close the field (hit) or "misses" if there was no successful closing of the field should be registered in the
+    game system. Repeat until "game over" appears in the game.',
+    'game_step_3' => '3. Analyze the results',
+    'game_step_3_description' => 'The game collects information about each square played in the game and has in-depth
+        progress / regression statistics for each of the sectors played in the game.',
+];
---
+++ darttrainer/lang/lv/welcome.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'game' => 'Pašmotivācijas šautriņu spēles apmācības palīgs',
+    'game_description' => 'Vai jums neliekās garlaicīgi pašam izvēlēties sektorus, uz kuriem mest šautriņu? Man personīgi
+        - ļoti! Tādēļ šī ir spēle, KURA NOTEIKS kurā lauciņā Tev jātrāpa. Tev atliek izvēlēties kāda tipa lauciņus vēlēsies
+        mest - parastos, dubultos, drīskāršos vai jebkuru. Sākot ar noteiktu spēļu skaitu, būs pieejama arī statistika par
+        precizitāti un progresu / regresu. Ja rodas kādi papildus jautājumi vai ierosinājumi, lūdzu sazināties, izmantojot
+        norādītos kontaktus.',
+    'game_step_1' => '1. Izvēlies sektoru',
+    'game_step_1_description' => 'Izvēlies spēles veidu - parastie sektori, dubultie sektori, trīskāršie sektori vai
+        jebkurš sektors. ',
+    'game_step_2' => '2. Desmit no desmit',
+    'game_step_2_description' => 'Spēle vienu pēc otra izvēlēsies kādu nejaušu lauciņu no jūsu izvēlētā diapozona.
+        Izvēļu (spēles gājienu) skaits: 10. Katram spēles gājienam maksimālais metienu mēģinājumu skaits: 10. Spēles
+        sistēmā jāreģistrē metienu skaits cik bijis nepieciešams lauciņa aizvēršanai (trāpījums) vai "garām", ja nav
+        bijusi veiksmīga lauciņa aizvēršana. Atkārtot līdz spēlē parādās uzraksts "spēle galā".',
+    'game_step_3' => '3. Analizē rezultātus',
+    'game_step_3_description' => 'Spēle ievāc informāciju par katru spēlē izspēlēto lauciņu un ir pieejama padziļināta
+        statistika par progresu / regresu par katru no spēlē spēlētajiem sektoriem.',
+];
