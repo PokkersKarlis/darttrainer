2c6ca1c8441ba3cd3ec5c735ec32501eb8d2286c
pokkers <karlis@pokkers.lv>
2024-07-06 18:21:53 +0300

    update text

--- darttrainer/resources/views/livewire/range-selector-info.blade.php
+++ darttrainer/resources/views/livewire/range-selector-info.blade.php
@@ -40,7 +40,7 @@
     @if(!Auth::check())
         <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
             <p class="font-bold">Play without registration</p>
-            <p>There will be statistics only for single game. If You want to collect statistics for every game, please register and use dashboard playground!</p>
+            <p>Game is based on user session. There will be statistics only for single game. If You want to collect statistics for every game, please register and use dashboard playground!</p>
         </div>
     @endif
     <button wire:click.prevent="startGame" class="button singles" type="submit">{{ $game_button }}</button>
