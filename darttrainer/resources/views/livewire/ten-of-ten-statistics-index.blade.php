c3557be552123110a9023e579db7c4b497fa0678
pokkers <karlis@pokkers.lv>
2024-07-21 16:47:42 +0300

    dashboard table test

--- darttrainer/resources/views/livewire/ten-of-ten-statistics-index.blade.php
+++ darttrainer/resources/views/livewire/ten-of-ten-statistics-index.blade.php
@@ -5,27 +5,39 @@
                 <table class="min-w-full bg-white border-collapse border border-gray-200">
                     <thead class="bg-gray-800 text-white">
                     <tr>
-                        <th class="text-left py-1 px-2 uppercase font-semibold text-xs border border-gray-200">Number</th>
-                        <th class="text-left py-1 px-2 uppercase font-semibold text-xs border border-gray-200">Single</th>
-                        <th class="text-left py-1 px-2 uppercase font-semibold text-xs border border-gray-200">Double</th>
-                        <th class="text-left py-1 px-2 uppercase font-semibold text-xs border border-gray-200">Triple</th>
+                        <th class="text-center py-2 px-2 uppercase font-semibold text-xs border border-gray-200">Number</th>
+                        <th class="text-center py-2 px-2 uppercase font-semibold text-xs border border-gray-200">Single</th>
+                        <th class="text-center py-2 px-2 uppercase font-semibold text-xs border border-gray-200">Double</th>
+                        <th class="text-center py-2 px-2 uppercase font-semibold text-xs border border-gray-200">Triple</th>
                     </tr>
                     </thead>
                     <tbody class="text-gray-700">
                     <tr>
-                        <td class="text-left py-3 px-4 border border-black">1</td>
+                        <td class="text-left py-3 px-4 border border-gray-200">
+                            <div class="py-2 px-2 uppercase font-bold text-xs border-black text-center bg-green-600 rounded border-2">
+                                1
+                            </div>
+                        </td>
                         <td class="text-left py-3 px-4 border border-gray-200">Data 1</td>
                         <td class="text-left py-3 px-4 border border-gray-200">Data 1</td>
                         <td class="text-left py-3 px-4 border border-gray-200">Data 1</td>
                     </tr>
                     <tr class="bg-gray-100">
-                        <td class="text-left py-3 px-4 border border-black">2</td>
+                        <td class="text-left py-3 px-4 border border-gray-200">
+                            <div class="py-2 px-2 uppercase font-bold text-xs border-black text-center bg-red-600 rounded border-2">
+                                2
+                            </div>
+                        </td>
                         <td class="text-left py-3 px-4 border border-gray-200">Data 2</td>
                         <td class="text-left py-3 px-4 border border-gray-200">Data 2</td>
                         <td class="text-left py-3 px-4 border border-gray-200">Data 2</td>
                     </tr>
                     <tr>
-                        <td class="text-left py-3 px-4 border border-black">3</td>
+                        <td class="text-left py-3 px-4 border border-gray-200">
+                            <div class="py-2 px-2 uppercase font-bold text-xs border-black text-center bg-red-600 rounded border-2">
+                                3
+                            </div>
+                        </td>
                         <td class="text-left py-3 px-4 border border-gray-200">Data 3</td>
                         <td class="text-left py-3 px-4 border border-gray-200">Data 3</td>
                         <td class="text-left py-3 px-4 border border-gray-200">Data 3</td>
